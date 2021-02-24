30149b81457810a6152dcaee98deed46a81fb1de
AyebakuroOruwori <ayebakurooruwori@gmail.com>
2021-02-25 00:29:16 +0100

    first commit

---
+++ datatypes.php
@@ -0,0 +1,27 @@
+<?php
+
+$string = "Hello World";
+
+echo $string . "<br>";
+
+$number = 847848;
+
+echo $number . "<br>";
+
+$float = 34.50;
+
+echo $float . "<br>";
+
+$boolean = false;
+
+echo $boolean  . "<br>";
+
+$name = array("jola", 4, "tope", "korede");
+
+var_dump($name);   
+
+echo "<br>";
+
+echo $name[2];
+
+?>
