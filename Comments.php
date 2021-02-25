c08b189b13deed3424bc3f44a40e2e87cb50666c
AyebakuroOruwori <ayebakurooruwori@gmail.com>
2021-02-25 02:31:09 +0100

    first commit

---
+++ Comments.php
@@ -0,0 +1,16 @@
+<?php
+ 
+# A program that discribe what a comment looks like and types of comment
+echo "This not a comment<br>";
+
+//echo "This is a comment";
+
+#  this is still a comment
+
+/*
+this is a code 
+this is another code
+*/
+
+echo "Ayebakuro";
+?>
